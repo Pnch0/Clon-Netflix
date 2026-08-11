a5975ee9f431bb40e6aaa6978a1982286c41455e
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-10 21:03:48 -0400

    feat: integracion y modificacion del archivo supabase.js

---
+++ Backend/Services/supabase.js
@@ -0,0 +1,17 @@
+import {createClient} from '@supabase/supabase.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL
+const anonKey = process.env.SUPABASE_KEY
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+
+export const supabase = createClient(supabaseUrl, anonKey);
+
+export const supabaseAdmin = createClient(supabaseUrl, serviceKey, {
+    auth:{
+        autoRefreshToken: false,
+        persistSession: false
+    }
+});
