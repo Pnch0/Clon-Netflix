c0a328f5496a2bea51a8da41291395ccc7afb436
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-11 19:27:49 -0400

    feat: implementacion de ruta para actualizar datos del usuario, archivo routes.users.js

--- Backend/Routes/routes.users.js
+++ Backend/Routes/routes.users.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { CreateUser } from '../Controllers/users.controllers.js';
+import { CreateUser, UpdateUser } from '../Controllers/users.controllers.js';
 
 const router = express.Router();
 
 router.post('/', CreateUser);
+router.put('/:id', UpdateUser);
 
 
 
