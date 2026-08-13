999f5bf4e561fb2b6d232693153f04c0d80277be
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-12 22:10:51 -0400

    feat: modificacion de App.jsx para el correcto funcionamiento del navbar, archivo App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import MainLayout from './Components/Layouts/MainLayout/MainLayout.jsx';
 function App() {
-  
+
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route element = {<MainLayout />}>
+        <Route path="/main-page" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
