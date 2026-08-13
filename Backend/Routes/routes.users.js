26466d445f9e2c39468488dfecdd48b3c4ebd530
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-12 21:48:48 -0400

    feat: implementacion de la ruta para obtener los datos de los usuarios, archivo routes.users.js

--- Backend/Routes/routes.users.js
+++ Backend/Routes/routes.users.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { CreateUser, UpdateUser } from '../Controllers/users.controllers.js';
+import { CreateUser, UpdateUser, GetUser } from '../Controllers/users.controllers.js';
 
 const router = express.Router();
 
 router.post('/', CreateUser);
+router.get('/', GetUser);
 router.put('/:id', UpdateUser);
+
 
 
 
