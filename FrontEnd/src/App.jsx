7d16bfa89dc0160a46941b62c459fc3ef329651d
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-13 20:16:51 -0400

    feat: creacion de navbar junto con su respectivo diseño, archivo Navbar.jsx - Navbar.css

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import MainLayout from './Components/Layouts/MainLayout/MainLayout.jsx';
+import MainPage from './Pages/MainPage/MainPage.jsx';
+
+
 function App() {
 
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element />
         <Route element = {<MainLayout />}>
         <Route path="/main-page" element={<MainPage />} />
         </Route>
--- FrontEnd/src/Components/Layouts/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Layouts/Navbar/Navbar.jsx
@@ -1,10 +1,37 @@
-
+import React from "react";
+import './Navbar.css';
+import { MdMovie } from "react-icons/md";
+import { FaUserCircle, FaSearch } from "react-icons/fa";
 
 function Navbar(){
 
     return(
         <>
-        
+        <div className="Contenedor-Navbar">
+            <div className="ContenedorNavbar-Izquierda">
+                <div className="ContenedorNavbarIzquierda-Izquierda">
+                    <MdMovie className = "Icono-Navbar"/>
+                </div>
+                <div className="ContenedorNavbarIzquierda-Derecha">
+                    <ul>
+                        <li>Home</li>
+                        <li>Series</li>
+                        <li>Peliculas</li>
+                        <li>Mi Lista</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="ContenedorNavbar-Derecha">
+                <div className="ContenedorNavbar-Input">
+                    <FaSearch className="Icono-Buscador" />
+                    <input 
+                    type="text" 
+                    placeholder="Titulos, personas, generos"
+                    />
+                </div>
+                <FaUserCircle className="Icono-Usuario"/>
+            </div>
+        </div>
         </>
     )
 }
