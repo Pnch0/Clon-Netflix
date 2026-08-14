47f8d6af45208f64fc8ad54bbc91c7924f76eef8
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-14 17:47:57 -0400

    feat: Creacion de Pagina Register junto con su respectivo diseño, archivos Register.jsx - Register.css

--- FrontEnd/src/Pages/RegisterPage/Register.jsx
+++ FrontEnd/src/Pages/RegisterPage/Register.jsx
@@ -1,10 +1,65 @@
+import './Register.css';
+import { NavLink } from 'react-router-dom';
 
 
 function RegisterPage(){
 
     return(
         <>
-        <h1>Register Page</h1>
+        <div className="Contenedor-Centrado">
+            <div className="Contenedor-Register">
+                <div className="ContenedorBotones-Register">
+                    <NavLink to="/" className="Boton-Login">
+                        Login
+                    </NavLink>
+                    <NavLink to="/register-page" className="Boton-Register">
+                        Register
+                    </NavLink>
+                </div>
+                <div className="ContenedorFormulario-Register">
+                    <form>
+                        <div className="Linea-Doble">
+                            <div className="Campo-Formulario">
+                                <label htmlFor="Nombre">Nombre:</label>
+                                <input 
+                                    type="text" 
+                                    id="Nombre" 
+                                    placeholder="Juan" 
+                                    required 
+                                />
+                            </div>
+                            <div className="Campo-Formulario">
+                                <label htmlFor="Apellido">Apellido:</label>
+                                <input 
+                                    type="text" 
+                                    id="Apellido" 
+                                    placeholder="Perez" 
+                                    required 
+                                />
+                            </div>
+                        </div>
+
+                        <label htmlFor="Email">Correo: </label>
+                        <input 
+                            type="email" 
+                            id='Email'
+                            placeholder='ejemplocorreo@gmail.com'
+                            required
+                        />
+
+                        <label htmlFor="Password">Contraseña: </label>
+                        <input 
+                            type="password" 
+                            id='Password'
+                            placeholder='*************'
+                            required
+                        />
+
+                        <button type='Submit' className='BotonSubmit-Register'>Iniciar Sesion</button>
+                    </form>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
