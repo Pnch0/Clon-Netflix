58e27037a918299ae4afa5bb47ec3d6c461c32a5
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-19 18:10:38 -0400

    feat: modificacion del archivo app.jsx para el correcto funcionamiento de las rutas protegidas

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,36 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import {Toaster} from 'sonner';
+import { Toaster } from 'sonner';
+import { ProtectedRoute, PublicRoute } from './Components/Routes/ProtectedRoute.jsx';
 import MainLayout from './Components/Layouts/MainLayout/MainLayout.jsx';
 import MainPage from './Pages/MainPage/MainPage.jsx';
 import LoginPage from './Pages/LoginPage/Login.jsx';
 import RegisterPage from './Pages/RegisterPage/Register.jsx';
 
-
 function App() {
-
-  return(
+  return (
     <>
-    
-    <Toaster 
+      <Toaster 
         position="top-right" 
         richColors 
         theme="dark" 
         closeButton 
       />
 
-    <Router>
-      <Routes>
-        <Route path="/" element = {<LoginPage />} />
-        <Route path="/register-page" element = {<RegisterPage />} />
+      <Router>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/register-page" element={<RegisterPage />} />
+          </Route>
 
-        <Route element = {<MainLayout />}>
-        <Route path="/main-page" element={<MainPage />} />
-        </Route>
-      </Routes>
-    </Router>
-
+          <Route element={<ProtectedRoute />}>
+            <Route element={<MainLayout />}>
+              <Route path="/main-page" element={<MainPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
