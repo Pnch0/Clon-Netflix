00c597fd20f5a48962bf18087a2d1d2db7b1919a
Pnch0 <francisco.espinozadi02@gmail.com>
2026-08-18 21:08:21 -0400

    feat: implementacion del UserService/createUser en el archivo Api.js

--- FrontEnd/src/Services/Api.js
+++ FrontEnd/src/Services/Api.js
@@ -22,3 +22,29 @@ export const AuthService = {
         return await response.json();
     },
 };
+
+
+export const UserService = {
+    createUser: async (userData) =>{
+        try{
+            const response = await fetch(`${API_URL}/users`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            if (!response.ok){
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Errro al registrar el usuario.');
+            }
+
+            return await response.json();
+        
+        } catch (error){
+            console.error("Error en el servicio createUser: ", error.message);
+            throw error;
+        }
+    },
+}
