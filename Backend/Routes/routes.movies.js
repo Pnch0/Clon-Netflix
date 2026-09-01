659a84d3893cdd6d2ff49147a37f4e405de4fd92
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-01 19:49:19 -0400

    feat: creacion archivo routes.movies junto con su respectivo codigo/funcionalidad

---
+++ Backend/Routes/routes.movies.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { GetTrending, GetMoviesByCategory, GetTvShowsByCategory, GetMovieDetails, GetTvDetails, DiscoverByGenre, GetGenres, SearchMulti } from '../Controllers/movies.controller.js';
+
+const router = express.Router();
+
+router.get('/trending', GetTrending);
+router.get('/movies/category/:category', GetMoviesByCategory);
+router.get('/tv/category/:category', GetTvShowsByCategory);
+router.get('/movie/:id', GetMovieDetails);
+router.get('/tv/:id', GetTvDetails);
+router.get('/discover/:mediaType/:genreId', DiscoverByGenre);
+router.get('/genres/:mediaType', GetGenres);
+router.get('/search', SearchMulti);
+
+export default router;
