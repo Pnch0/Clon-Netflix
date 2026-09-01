49ad8579edd0064de0871711bb41ae70b4964d84
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-01 19:51:45 -0400

    feat: implemetancion de routeMovies, archivo index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { supabase } from './Services/supabase.js';
 
+import routeMovies from './Routes/routes.movies.js'
 import routeUsers from './Routes/routes.users.js';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', routeUsers);
+app.use('/api', routeMovies);
 
 const PORT = 3000;
 
