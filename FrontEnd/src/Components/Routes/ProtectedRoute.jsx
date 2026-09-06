73cd6603b1289d8846434cf90198e73a007611ca
Pnch0 <francisco.espinozadi02@gmail.com>
2026-09-06 20:28:34 -0300

    fix: modificacion de ProtectedRoute.jsx

--- FrontEnd/src/Components/Routes/ProtectedRoute.jsx
+++ FrontEnd/src/Components/Routes/ProtectedRoute.jsx
@@ -3,14 +3,15 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const ProtectedRoute = () => {
   const token = localStorage.getItem('token');
 
+  const isAuthenticated = token && token !== 'null' && token !== 'undefined';
 
-  return token ? <Outlet /> : <Navigate to="/" replace />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 };
-
 
 export const PublicRoute = () => {
   const token = localStorage.getItem('token');
-
   
-  return !token ? <Outlet /> : <Navigate to="/main-page" replace />;
+  const isAuthenticated = token && token !== 'null' && token !== 'undefined';
+
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/main-page" replace />;
 };
